779680b52601c3d95bf57c8e03a1e4a8eafd971c
karsh <utkarsh@cyberfear.com>
2025-05-26 14:17:17 +1000

    global/local fix

--- scripts/control.py
+++ scripts/control.py
@@ -16,22 +16,37 @@ old_s3_temperature = ""
 #- Public Functions --------------------------------------------------------------------------------
 
 def get_s1_message():
+    global s1_motor
+    global old_s1_motor
+
     if s1_motor != old_s1_motor:
         old_s1_motor = s1_motor
         return "m"
+
+    global s1_temperature_threshold
+    global old_s1_temperature_threshold
 
     if s1_temperature_threshold != old_s1_temperature_threshold:
         old_s1_temperature_threshold = s1_temperature_threshold
         return f"t{s1_temperature_threshold}"
 
+
 def clear_s1_message():
+    global s1_temperature_threshold
+    global old_s1_temperature_threshold
+
     old_s1_temperature_threshold = s1_temperature_threshold
     old_s1_motor = s1_motor
 
+
 def get_s2_moisture_threshold():
-    return buzzer_threshold
+    return s2_moisture_threshold
+
 
 def get_s3_message():
+    global s3_temperature
+    global old_s3_temperature
+
     if s3_temperature != old_s3_temperature:
         old_s3_temperature = s3_temperature
         return s3_temperature
